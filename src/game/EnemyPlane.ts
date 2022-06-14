fd3d68148eb896e5425ee4fd0a9491d218e29144
hyperMoss <gooingyu@outlook.com>
2022-06-14 23:49:51 +0800

    feat: destory enemy

--- src/game/EnemyPlane.ts
+++ src/game/EnemyPlane.ts
@@ -2,18 +2,18 @@ export class EnemyPlane {
   public x: number = 0
   public y: number = 0
   public speed: number = 1
-  public border: number = 0;
+  public border: number = 400;
   public width: number = 0
   public height: number = 0
-  // public onDestroy: any;
+  public onDestroy: any;
   constructor() {
   }
 
   move() {
     this.y += this.speed
 
-    if (this.y < this.border) {
-      // this.onDestroy && this.onDestroy();
+    if (this.y > this.border) {
+      this.onDestroy && this.onDestroy();
     }
   }
 
@@ -31,7 +31,9 @@ export function runEnemyPlanes(enemyPlanes: EnemyPlane[]) {
 
 
   enemyPlanes.forEach(enemy => {
+    enemy.onDestroy = enemyPlanes.splice(enemyPlanes.indexOf(enemy), 1)
     enemy.move()
+
   })
 
 }
